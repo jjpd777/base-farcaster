c81560dbb9d0e245aadc5f3ae2b88ee964dd45e4
Juan-JoseJos418 <14027448-Juan-JoseJos418@users.noreply.replit.com>
2025-03-01 00:53:20 +0000

    User checkpoint: Add route displaying login functionality.

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
-
 export const BASE_SEPOLIA_CHAIN_ID = 84532;
-export const mintContractAddress = '0x83bd615eb93eE1336acA53e185b03B54fF4A17e8';
+export const mintContractAddress = '0xA3e40bBe8E8579Cd2619Ef9C6fEA362b760dac9f';
 export const mintABI = [
   {
     inputs: [
@@ -10,7 +9,7 @@ export const mintABI = [
         type: 'address',
       },
     ],
-    name: 'mintTo',
+    name: 'mint',
     outputs: [],
     stateMutability: 'public',
     type: 'function',
